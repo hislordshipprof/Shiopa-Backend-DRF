346c52033e7d428ff8f8c405998051536949de73
sheyziSilver <blogsedap@gmail.com>
2021-06-18 10:12:14 +0100

    Completed the product models with the admin panel interface

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from apps.core.models import Image, Section
 
+
 admin.site.register(Image)
 admin.site.register(Section)
--- apps/product/admin.py
+++ apps/product/admin.py
@@ -1,5 +1,29 @@
 from django.contrib import admin
-from apps.product.models import Category
+from apps.product.models import Category, Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'sku',
+                    'available_to_purchase', 'created_at')
+    list_filter = ('name', 'price', 'available_to_purchase',
+                   'created_at')
+    filter_horizontal = ('images', 'section')
+
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'slug', 'description', 'additional_info')
+        }),
+        ('Price', {
+            'fields': ('price', 'discount_price')
+        }),
+        ('Images', {
+            'fields': ('main_image', 'images')
+        }),
+        ('Stock', {
+            'fields': ('sku', 'available_to_purchase')
+        })
+
+    )
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -8,3 +32,4 @@ class CategoryAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
